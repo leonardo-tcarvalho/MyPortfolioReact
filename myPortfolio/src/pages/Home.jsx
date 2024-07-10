51bff0034b7488025d59410b530f8e074184c461
Leonardo Carvalho <leonardotadeu.carvalho@gmail.com>
2024-07-10 02:11:43 -0300

    Update: Home.jsx

--- myPortfolio/src/pages/Home.jsx
+++ myPortfolio/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export function Home() {
     return (
-        <div className="w-full min-h-[40rem] rounded-2xl border-1 border-zinc-700 bg-zinc-800">
+        <div className="w-full min-h-[40rem] max-h-[40rem] overflow-y-auto rounded-2xl border-1 border-zinc-700 bg-zinc-800">
             <nav className="flex w-full justify-end">
                 <ul className="flex justify-center items-center w-2/4 h-10 bg-zinc-800 border-zinc-700 border-1 rounded-tr-2xl rounded-bl-2xl shadow-5">
                     <Link className="w-1/3 h-full" to="/">
