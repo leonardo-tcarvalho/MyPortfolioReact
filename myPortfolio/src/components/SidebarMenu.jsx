0b3d49091f848456121bbae5cd8b2306c14b6eba
Leonardo Carvalho <leonardotadeu.carvalho@gmail.com>
2024-08-25 13:36:21 -0300

    style: fix gap

--- myPortfolio/src/components/SidebarMenu.jsx
+++ myPortfolio/src/components/SidebarMenu.jsx
@@ -7,15 +7,16 @@ import { Github } from "../images/Github";
 export function SidebarMenu() {
   return (
     <aside className="flex flex-col items-center w-full p-4 mt-5 bg-zinc-800">
-      <Avatar />
-      <p className="text-center text-zinc-300 text-2xl font-bold">
-        Leonardo Carvalho
-      </p>
-      <div className="mt-4 bg-purple-600 px-4 py-1 rounded-md shadow-md">
-        <p className="text-zinc-200 ">Desenvolvedor Web</p>
+      <div className="flex flex-col items-center gap-5">
+        <Avatar />
+        <p className="text-center text-zinc-300 text-3xl font-bold">
+          Leonardo Carvalho
+        </p>
+        <div className="bg-purple-600 px-4 py-1 rounded-md shadow-md">
+          <p className="text-center text-zinc-200 ">Desenvolvedor Web</p>
+        </div>
       </div>
       <span className="w-full h-1 my-5 border-b-1 border-zinc-700"></span>
-      <div className="flex flex-col w-full gap-5"></div>
       <div className="flex justify-center gap-4 w-full">
         <a href="https://www.instagram.com/cabelozl/" target="_blank">
           <Instagram />
@@ -35,9 +36,9 @@ export function SidebarMenu() {
       "
       >
         <Link to="/">
-          <p className="max-sm:w-52 w-96 text-center px-4 py-2 text-xl text-zinc-300 bg-purple-600 rounded-md">
-            Sobre
-          </p>
+          <div className="max-sm:w-52 w-96 text-center px-4 py-2 bg-purple-600 rounded-md hover:shadow-inner hover:bg-white hover:duration-300">
+            <p className="text-xl text-zinc-300">Sobre</p>
+          </div>
         </Link>
         <Link to="/skills">
           <p className="max-sm:w-52 w-96 text-center px-4 py-2 text-xl text-zinc-300 bg-purple-600 rounded-md">
--- myPortfolio/vite.config.js
+++ myPortfolio/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Ou use '0.0.0.0' para ouvir em todas as interfaces
+    port: 5173,
+  },
+});
