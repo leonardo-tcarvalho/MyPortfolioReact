2368b8ce1cc87468aaf7e9e702c823e5692e9ceb
cabeleozl <leonardocarvalho.wk@gmail.cm>
2024-12-17 03:06:10 -0300

    feat: image sidebar

--- myPortfolio/src/components/Avatar.jsx
+++ myPortfolio/src/components/Avatar.jsx
@@ -1,8 +1,8 @@
-import AvatarImage from "../images/Avatar.png";
+import AvatarImage from "../images/Avatar2.jpeg";
 
 export function Avatar() {
   return (
-    <div className="flex items-endrelative max-lg:w-52 max-lg:h-52 w-36 h-36 mt-5 rounded-3xl px-1 shadow-10 bg-cover object-center">
+    <div className="flex items-endrelative max-lg:w-52 max-lg:h-52 w-36 h-36 mt-5 rounded-3xl shadow-10 bg-cover object-center">
       <img className="rounded-2.5xl drop-shadow-10" src={AvatarImage} alt="" />
     </div>
   );
