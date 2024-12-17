eb022d93e2d26c4c646659f1dc1ea0a2e8a2fe06
cabeleozl <leonardocarvalho.wk@gmail.cm>
2024-12-17 02:43:11 -0300

    fix: url images new

--- myPortfolio/src/images/Instagram.jsx
+++ myPortfolio/src/images/Instagram.jsx
@@ -1,5 +1,19 @@
 export function Instagram() {
-    return (
-        <svg className="hover:scale-110 hover:duration-300 duration-200 stroke-purple-500 hover:stroke-zinc-300" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
-    )
+  return (
+    <svg
+      className="hover:scale-110 hover:duration-300 duration-200 stroke-purple-500 hover:stroke-zinc-300"
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      fill="none"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+      <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+      <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+    </svg>
+  );
 }
